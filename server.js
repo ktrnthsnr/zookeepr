2ea845a76998c4c7b0b6d686b6310220fb8339ae
ktrnthsnr <asakthisner@hotmail.com>
2020-05-27 17:44:54 -0700

    added startup frontend code and app.get sendFile path

--- server.js
+++ server.js
@@ -145,6 +145,10 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
       // -- POST requests
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
